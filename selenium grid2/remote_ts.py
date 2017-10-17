14eb02e55d128e1b2647e28c20c0d6b1ec55dca0
friendnj <810455341@qq.com>
2017-10-17 19:02:04 +0800

    1017
    1017

---
+++ selenium grid2/remote_ts.py
@@ -0,0 +1,13 @@
+from selenium.webdriver import Remote
+
+driver=Remote(command_executor='http://127.0.0.1:4444/wd/hub',
+              desired_capabilities={'platform':'ANY',
+                                    'browserName':'firefox',
+                                    'version':'',
+                                    'javascriptEnabled':True
+                                    }
+              )
+
+driver.get('http://www.baidu.com')
+driver.find_element_by_id("kw").send_keys("remote")
+driver.find_element_by_id("su").click()
